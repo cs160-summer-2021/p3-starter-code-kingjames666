57adb7dc9b78747acd90f67a458eb69db5bc1853
yixin chen <you@example.com>
2021-07-12 14:17:53 -0700

    part of UI

--- coloring/urls.py
+++ coloring/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('demo', views.index, name='demo'),
-    path('new_interaction', views.index, name='new_interaction')
+    path('new_interaction', views.index, name='new_interaction'),
+    path('frame1', views.frame1, name='frame1'),
+    path('frame2', views.frame2, name='frame2')
 ]
--- coloring/views.py
+++ coloring/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
+def frame1(request):
+    return render(request, 'coloring/frame1.html')
+
+def frame2(request):
+    return render(request, 'coloring/frame2.html')
+
 def index(request):
     return render(request, 'coloring/index.html')
